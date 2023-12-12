f8665e51f481d369faf93c19de4863934eb5ec25
amandanitta <amandanitta458@gmail.com>
2023-12-12 13:34:20 -1000

    updated vis.py

--- vis.py
+++ vis.py
@@ -1,10 +1,10 @@
 import json
 import subprocess
 
-# try:
-#     subprocess.run(['/bin/bash', 'install_packages.sh'], check=True)
-# except subprocess.CalledProcessError as e:
-#     print(f"Error running shell script: {e}")
+try:
+    subprocess.run(['/bin/bash', 'install_packages.sh'], check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error running shell script: {e}")
 
 import streamlit as st
 import pandas as pd
