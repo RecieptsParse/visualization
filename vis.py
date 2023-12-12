5df5b181c898683c00c788d0378d8b0ddf558576
amandanitta <amandanitta458@gmail.com>
2023-12-11 19:53:30 -1000

    added imports

--- vis.py
+++ vis.py
@@ -2,13 +2,14 @@ import os
 import json
 import sys
 import subprocess
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 
 try:
     subprocess.run(['/bin/bash', 'install_packages.sh'], check=True)
 except subprocess.CalledProcessError as e:
     print(f"Error running shell script: {e}")
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
 st.write("MY APP")
